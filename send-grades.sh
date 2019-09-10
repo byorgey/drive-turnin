d664aae52a6f11c995fe8bed41caf5805c2cfcb9
Brent Yorgey <byorgey@gmail.com>
2019-09-10 10:14:08 -0500

    rsync -> aws

--- send-grades.sh
+++ send-grades.sh
@@ -2,4 +2,4 @@
 
 ./get-submissions.py "${1}g"
 ./format_grades.py gradebook-$1.csv ../encrypt/students/
-cd ../encrypt && ./publish.py keys.txt students students-enc && rsync -r students-enc/* ozark:ozark/encrypted/
+cd ../encrypt && ./publish.py keys.txt students students-enc && aws s3 sync students-enc/ s3://ozark.hendrix.edu/~yorgey/encrypted/
