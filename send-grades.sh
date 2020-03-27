bd7ea34368903645bb5917523ba76f65707c5d19
Brent Yorgey <byorgey@gmail.com>
2020-03-27 07:19:50 -0500

    add note on how to email grades

--- send-grades.sh
+++ send-grades.sh
@@ -2,3 +2,7 @@
 
 ./get-submissions.py "${1}g"
 ./format_grades.py gradebook-$1.csv
+
+# To actually send grades, go into directory with generated student folders and
+#
+#   for d in *(/); do msmtp -t < $d/*.txt; done
