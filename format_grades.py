54444e217872ceae0f4831ad6fa1a7931834beb8
Brent Yorgey <byorgey@gmail.com>
2021-03-16 05:51:15 -0500

    generalize grade formatting tool

--- format_grades.py
+++ format_grades.py
@@ -4,24 +4,28 @@ import re
 import os
 import sys
 import csv
+from pathlib import Path
 
 CURSEMESTER = '21G'
 
 if len(sys.argv) < 2:
-    print("Usage: format_grades.py <gradebook> [<message file>] [<output dir>]")
+    print("Usage: format_grades.py <gradebook> [<message file>] [<subject>] [<output dir>]")
     sys.exit(0)
 
 gradebook_file = sys.argv[1]
 
-classNum = re.match(r".*-(M?\d+)\.csv", gradebook_file).group(1)
+classNumMatch = re.match(r".*-(M?\d+)\.csv", gradebook_file)
+if classNumMatch is not None:
+    classNum = classNumMatch.group(1)
+else:
+    classNum = 'unknown'
 
-
-if len(sys.argv) < 4:
+if len(sys.argv) < 5:
     outdir = f'/home/brent/teaching/{classNum}/{CURSEMESTER}/grades/'
     print(f'Defaulting to output dir {outdir}...')
 else:
-    print(f'Using custom output dir {sys.argv[3]}...')
-    outdir = sys.argv[3]
+    print(f'Using custom output dir {sys.argv[4]}...')
+    outdir = sys.argv[4]
 
 classNames =\
     { '365': 'Functional Programming',
@@ -30,7 +34,8 @@ classNames =\
       '150': 'CSCI 150',
       '151': 'Data Structures',
       '360': 'Programming Languages',
-      '382': 'Algorithms'
+      '382': 'Algorithms',
+      'unknown': 'Unknown'
     }
 
 className: str = classNames[classNum]
@@ -42,6 +47,20 @@ else:
     print(f'Loading message from {sys.argv[2]}...')
     with open(sys.argv[2], 'r') as msgfile:
         message = msgfile.read()
+
+if len(sys.argv) < 4:
+    print('Using default subject...')
+    subject = ''
+else:
+    subject = sys.argv[3]
+    print(f'Using custom subject: {subject}')
+
+alias_dict = {}
+with Path("~/.aliases/stu.aliases").expanduser().open() as aliases:
+    for alias in aliases:
+        m = re.search(r"\"(.*) <(.*)>", alias)
+        if m:
+            alias_dict[m.group(1).lower()] = m.group(2)
 
 with open(gradebook_file, 'r') as gradebook:
     lineCount = 0
@@ -60,16 +79,36 @@ with open(gradebook_file, 'r') as gradebook:
         lineCount += 1
         if lineCount == 1:
             header = entry
-            emailColumn: int = entry.index('Email')
-            nameColumn: int = entry.index('Preferred')
+            if 'Email' in entry:
+                emailColumn: int = entry.index('Email')
+            else:
+                emailColumn = None
+
+            if 'Preferred' in entry:
+                nameColumn: int = entry.index('Preferred')
+            elif 'Name' in entry:
+                nameColumn: int = entry.index('Name')
+            else:
+                nameColumn = None
 
             try:
                 droppedColumn: int = entry.index('Dropped (!)')
             except ValueError:
                 droppedColumn = -1
         else:
-            email = entry[emailColumn]
-            name  = entry[nameColumn]
+            if emailColumn is not None:
+                email = entry[emailColumn]
+
+            if nameColumn is not None:
+                name = entry[nameColumn].strip()
+
+            if emailColumn is None and nameColumn is not None:
+                if name.lower() in alias_dict:
+                    email = alias_dict[name.lower()]
+                else:
+                    print(f'Warning, {name} not found in alias dict.')
+                    email = ''
+
             if droppedColumn != -1:
                 dropped = entry[droppedColumn]
 
@@ -83,11 +122,14 @@ with open(gradebook_file, 'r') as gradebook:
 
                 with open("%s/%s-grades.txt" % (d, classNum), 'w') as student_file:
 
-                    student_file.write(f'Subject: Current {className} grades\n')
+                    if subject == '':
+                        student_file.write(f'Subject: Current {className} grades\n')
+                    else:
+                        student_file.write(f'Subject: {subject}\n')
                     student_file.write(f'To: {email}\n')
                     student_file.write(f'From: Brent Yorgey <yorgey@hendrix.edu>\n\n')
 
-                    student_file.write(f'Dear {name},\n\n')
+                    student_file.write(f'Dear {name.split()[0]},\n\n')
                     student_file.write(message)
                     for i in range(len(header)):
                         if ('(!)' not in header[i] and header[i] != ''):
