5290bf77634c624445f998110ce824d37ebaee14
Brent Yorgey <byorgey@gmail.com>
2023-02-20 12:14:45 -0600

    update current semester

--- format_grades.py
+++ format_grades.py
@@ -6,7 +6,7 @@ import sys
 import csv
 from pathlib import Path
 
-CURSEMESTER = '22L'
+CURSEMESTER = '23G'
 
 if len(sys.argv) < 2:
     print("Usage: format_grades.py <gradebook> [<message file>] [<subject>] [<output dir>]")
