7322d8d43554a8abc2dc84c3de8c8be4ad72b909
Brent Yorgey <byorgey@gmail.com>
2025-01-25 13:47:22 -0600

    now 25G semester

--- format_grades.py
+++ format_grades.py
@@ -6,7 +6,7 @@ import sys
 import csv
 from pathlib import Path
 
-CURSEMESTER = '24G'
+CURSEMESTER = '25G'
 
 if len(sys.argv) < 2:
     print("Usage: format_grades.py <gradebook> [<message file>] [<subject>] [<output dir>]")
